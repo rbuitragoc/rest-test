4f90e03d22378351a1e9696e31c48538534cfc32
Rick Buitrago <rbuitragoc@gmail.com>
2017-11-28 19:05:12 -0500

    added greetings/{user} @Path

--- src/main/java/com/me/rest/GreetingsResource.java
+++ src/main/java/com/me/rest/GreetingsResource.java
@@ -4,6 +4,7 @@ import javax.json.Json;
 import javax.json.JsonObject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 
 @Path("greetings")
 public class GreetingsResource {
@@ -12,5 +13,10 @@ public class GreetingsResource {
     @GET
     public JsonObject greetings() {
         return Json.createObjectBuilder().add("salutation", "hello world!").build();
+    }
+    @GET
+    @Path("/{user}")
+    public JsonObject helloUser(@PathParam(value="user") String user) {
+        return Json.createObjectBuilder().add("salutation", String.format("hello, %s", user)).build();
     }
 }
